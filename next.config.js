ed5d8a872cdb87d87975d4f02a2f3449df1a2046
jay <jayvaghani2000@gmail.com>
2023-11-24 17:37:29 +0530

    finish setup pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+
+// Configuration options for Next.js
 const nextConfig = {
   webpack(config) {
     config.module.rules.push({
@@ -13,4 +15,13 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// Configuration object tells the next-pwa plugin
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+// Export the combined configuration for Next.js with PWA support
+module.exports = withPWA(nextConfig);
