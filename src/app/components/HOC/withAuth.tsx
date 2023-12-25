eee23fcf611c7cc89b41829dc3daaffeb05bb3b3
jay vaghani <jayvaghani2000@gmail.com>
2023-12-25 14:35:41 +0530

    add current session logic

--- src/app/components/HOC/withAuth.tsx
+++ src/app/components/HOC/withAuth.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch } from "@/app/store";
 import { setAuthData, useAuthStore } from "@/app/store/authentication";
 import { handleSetToken } from "@/app/utils/handleSetToken";
+import { Session } from "@supabase/supabase-js";
 import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { supabase } from "../../../../supabase/init";
@@ -25,35 +26,54 @@ const WithAuth = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useAppDispatch();
 
   const handleCreateSession = async () => {
-    const { data } = await supabase.auth.refreshSession();
-    if (data && data.session) {
-      await handleSetToken(data.session.access_token);
-      const res = await supabase.auth.setSession({
-        access_token: data.session.access_token,
-        refresh_token: data.session.refresh_token,
-      });
-      if (res.data.user && res.data.session) {
-        dispatch(
-          setAuthData({
-            profile: res.data.user,
-            authenticated: true,
-            token: res.data.session.access_token,
-            authCheck: true,
-          })
-        );
-      }
+    const currentSession = await supabase.auth.getSession();
+    const { data: currentSessionData, error } = currentSession;
+
+    if (currentSessionData && currentSessionData.session && error) {
+      const session = currentSessionData.session as Session;
+      dispatch(
+        setAuthData({
+          profile: session.user,
+          authenticated: true,
+          token: session.access_token,
+          authCheck: true,
+        })
+      );
+
       if (loginRoute.includes(path!)) {
         router.replace("/");
       }
     } else {
-      if (authRoute.includes(path!)) {
-        router.replace("/");
+      const { data } = await supabase.auth.refreshSession();
+      if (data && data.session) {
+        await handleSetToken(data.session.access_token);
+        const res = await supabase.auth.setSession({
+          access_token: data.session.access_token,
+          refresh_token: data.session.refresh_token,
+        });
+        if (res.data.user && res.data.session) {
+          dispatch(
+            setAuthData({
+              profile: res.data.user,
+              authenticated: true,
+              token: res.data.session.access_token,
+              authCheck: true,
+            })
+          );
+        }
+        if (loginRoute.includes(path!)) {
+          router.replace("/");
+        }
+      } else {
+        if (authRoute.includes(path!)) {
+          router.replace("/");
+        }
+        dispatch(
+          setAuthData({
+            authCheck: true,
+          })
+        );
       }
-      dispatch(
-        setAuthData({
-          authCheck: true,
-        })
-      );
     }
   };
 
