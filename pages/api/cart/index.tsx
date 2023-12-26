ec110a6c8d5cbf8c0309feedd6b317a2bc1c6546
jay vaghani <jayvaghani2000@gmail.com>
2023-12-26 23:40:27 +0530

    return error

--- pages/api/cart/index.tsx
+++ pages/api/cart/index.tsx
@@ -25,7 +25,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse, user: any) {
       const errorObj = handleErrorMsg(error);
       return res.status(400).json({ error: errorObj });
     }
-    return res.status(500).json({ error: "Something went wrong" });
+    return res.status(500).json({ error: error });
   }
 }
 
