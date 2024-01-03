90c3e5e01a5508203aa4edfa950c610af26381e8
jay <jayvaghani2000@gmail.com>
2024-01-03 19:00:46 +0530

    display cart items

--- src/app/components/Cart/CartDetail/index.tsx
+++ src/app/components/Cart/CartDetail/index.tsx
@@ -1,9 +1,16 @@
+import { useCartStore } from "@/app/store/cart";
+import { useVisitorCartStore } from "@/app/store/visitorCart";
 import Stepper from "./Stepper";
 
 const CartDetail = () => {
+  const { cart } = useCartStore();
+  const { cart: visitorCart } = useVisitorCartStore();
+
   return (
     <div className="my-[30px] sm:my-[60px]">
       <Stepper />
+
+      {JSON.stringify(visitorCart)}
     </div>
   );
 };
