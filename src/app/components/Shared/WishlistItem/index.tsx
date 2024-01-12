d81b7d0bb36177fabcd3ab7f1095734550ff49d5
jay vaghani <jayvaghani2000@2gmail.com>
2024-01-12 18:39:31 +0530

    show login popup

--- src/app/components/Shared/WishlistItem/index.tsx
+++ src/app/components/Shared/WishlistItem/index.tsx
@@ -1,15 +1,31 @@
+import { useAppDispatch } from "@/app/store";
+import { useAuthStore } from "@/app/store/authentication";
+import { setGlobalData } from "@/app/store/global";
 import classNames from "classnames";
 import Icon from "../../Icon";
 import styles from "./wishlistItem.module.scss";
 
 type propType = {
   favorite?: boolean;
+  onClick?: () => void;
 };
 
 const WishlistItem = (props: propType) => {
-  const { favorite = true } = props;
+  const { favorite = true, onClick = () => {} } = props;
+  const dispatch = useAppDispatch();
+  const { authenticated } = useAuthStore();
+
   return (
-    <button className="flex justify-center items-center h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7 p-[5px] sm:p-[6px] rounded-full bg-darkGray">
+    <button
+      className="flex justify-center items-center h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7 p-[5px] sm:p-[6px] rounded-full bg-darkGray"
+      onClick={() => {
+        if (!authenticated) {
+          dispatch(setGlobalData({ showLogin: true }));
+        } else {
+          onClick();
+        }
+      }}
+    >
       <Icon
         name="heart"
         className={classNames("h-full w-full", {
