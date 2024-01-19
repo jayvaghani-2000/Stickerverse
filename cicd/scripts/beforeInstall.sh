7c190df1b5455f9bcddb7afc7ad8bd4cee58c399
bhupalnitw@gmail.com <you@example.com>
2024-01-19 23:53:11 +0530

    changed the file

--- cicd/scripts/beforeInstall.sh
+++ cicd/scripts/beforeInstall.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-pm2 stop all
-pm2 delete all
-cd /home/ubuntu/sticker-verse
-
-rm -r node_modules
-
 cd /home/ubuntu/
-rm -rf sticker-verse-$(date + "%Y")*.zip
-zip -r sticker-verse-$(date +"%Y-%m-%d::%H:%M:%S").zip sticker-verse/
-
 rm -rf sticker-verse
