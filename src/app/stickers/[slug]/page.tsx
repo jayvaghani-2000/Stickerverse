4b42af3e43b33cda5a4e9ea4e1780f2051d62120
Avi kheni <avikheni@applaunch.io>
2024-04-06 18:25:08 +0530

    update seo image in sticker detail page

--- src/app/stickers/[slug]/page.tsx
+++ src/app/stickers/[slug]/page.tsx
@@ -7,6 +7,25 @@ export async function generateMetadata(prop: propType) {
   const { data } = await getStickerDetails(prop.params.slug);
   return {
     title: data?.productName,
+    icons: { apple: "/icon.png" },
+    twitter: {
+      card: "summary_large_image",
+      title: data?.productName,
+      description:
+        "Explore StickerVerse, where every amusing, eccentric, and side-splitting sticker comes to life! We've got a vast collection of funny stickers for you to discover and enjoy.",
+      images: [
+        {
+          url: data?.image[0].url,
+        },
+      ],
+    },
+    openGraph: {
+      type: "website",
+      title: data?.productName,
+      description:
+        "Explore StickerVerse, where every amusing, eccentric, and side-splitting sticker comes to life! We've got a vast collection of funny stickers for you to discover and enjoy.",
+      images: [{ url: data?.image[0].url }],
+    },
   };
 }
 
